458ce2e50181b1b3803fe9037aca565ca17fbf90
V4Vince <vince.guoo@gmail.com>
2016-08-02 23:29:51 -0400

    splits house post action into two
    -one in the new-property route
    -one in the new-form component

--- app/components/new-form/component.js
+++ app/components/new-form/component.js
@@ -3,8 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   flashMessages: Ember.inject.service(),
-  store: Ember.inject.service(),
-  router: Ember.inject.service('-routing'),
+
   tagName: 'form',
 
   houseParams: {},
@@ -13,22 +12,12 @@ export default Ember.Component.extend({
 //1. When creating a record, ember data creates duplicate records;
 //   one that has an ID and is saved on the server
 //   one that has NO ID and is not saved on the server
+
+//Sends action to new-property route
   actions: {
-
     submit () {
-      let houseParams = this.get('houseParams');
-      let newHouse = this.get('store').createRecord('house', houseParams);
-      return newHouse.save()
-      .then(() => this.get('flashMessages').success('You created a new property'))
-      .then(() => {
-        this.get('router').transitionTo('houses');
-      })
-      .catch(() => {
-        this.get('flashMessages')
-        .danger('There was a problem. Please try again.');
-      });
+      this.sendAction('submit', this.get('houseParams'));
     },
-
     reset () {
       this.set('houseParams', {});
       console.log("hi");
--- app/new-property/route.js
+++ app/new-property/route.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  //Gets data from new-form componenet
+  actions: {
+    createHouse(data){
+      let house = this.get('store').createRecord('house', data);
+      return house.save()
+      .then(() => this.get('flashMessages').success('You created a new property'))
+      .then(() => {
+        this.transitionTo('houses');
+      })
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('There was a problem. Please try again.');
+      });
+    },
+  },
 });
