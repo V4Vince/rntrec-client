58411e11425583c3ef311e87f16bd95f8748bf9b
V4Vince <vince.guoo@gmail.com>
2016-08-05 17:07:27 -0400

    Adds computed sort property to all-property view

--- app/components/my-houses/component.js
+++ app/components/my-houses/component.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  //housesData <- from houses route
+
+  sortPropertyBy: ['street_name'],
+  sortedProperty: Ember.computed.sort('housesData', 'sortPropertyBy'),
+  
 });
