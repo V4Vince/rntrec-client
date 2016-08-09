79852bf43b91025c62dd5f91a97651df5bd154fc
V4Vince <vince.guoo@gmail.com>
2016-08-09 17:32:29 -0400

    Adds contract as modal in unit details
    -Rename path names for houses to properties

--- app/components/contract-card/component.js
+++ app/components/contract-card/component.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  //contract <- from contract route
+
+  actions: {
+
+    closeModal: function(){
+      this.set('showContract', false);
+    },
+  },
+
 });
--- app/router.js
+++ app/router.js
@@ -10,8 +10,8 @@ Router.map(function () {
   this.route('sign-in');
   this.route('change-password');
   this.route('users');
-  this.route('houses');
-  this.route('house', { path: '/houses/:house_id' });
+  this.route('houses', { path: '/properties' });
+  this.route('house', { path: '/properties/:house_id' });
   this.route('units');
   this.route('unit', { path: '/units/:unit_id' });
   this.route('new-property');
