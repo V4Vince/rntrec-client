432c84c1aa2c9d4bc71f59030d1673d5d80b786e
V4Vince <vince.guoo@gmail.com>
2016-08-11 23:57:07 -0400

    Added notes and bookmarks in the code
    -Refer to previous commit - no changes.

--- app/components/contract-card/component.js
+++ app/components/contract-card/component.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  //unitData <- from unit-details component
 
   actions: {
 
+    load: function(){
+      let unit = this.get('unitData');
+      console.log(unit);
+      let contract = unit.get('contract');
+      console.log(contract);
+    },
     closeModal: function(){
       this.set('showContract', false);
     },
