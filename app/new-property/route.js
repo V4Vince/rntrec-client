bb392c8156e6513a01f9528fbb2b2f1d743a8125
V4Vince <vince.guoo@gmail.com>
2016-08-11 21:39:52 -0400

    Moves component for adding new properties
    -Injects new-property/building component into new-property route.
    -Will delete new-form

--- app/new-property/route.js
+++ app/new-property/route.js
@@ -3,19 +3,20 @@ import ScrollPositionMixin from '../mixins/scroll-position';
 
 export default Ember.Route.extend(ScrollPositionMixin, {
 
+
   //Gets data from new-form componenet
-  actions: {
-    createHouse(data){
-      let house = this.get('store').createRecord('house', data);
-      return house.save()
-      .then(() => this.get('flashMessages').success('You created a new property'))
-      .then(() => {
-        this.transitionTo('houses');
-      })
-      .catch(() => {
-        this.get('flashMessages')
-        .danger('There was a problem. Please try again.');
-      });
-    },
-  },
+  // actions: {
+  //   createHouse(data){
+  //     let house = this.get('store').createRecord('house', data);
+  //     return house.save()
+  //     .then(() => this.get('flashMessages').success('You created a new property'))
+  //     .then(() => {
+  //       this.transitionTo('houses');
+  //     })
+  //     .catch(() => {
+  //       this.get('flashMessages')
+  //       .danger('There was a problem. Please try again.');
+  //     });
+  //   },
+  // },
 });
