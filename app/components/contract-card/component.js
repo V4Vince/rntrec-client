63e6b37a7f62a5a51df3152ec73a41bcf053a045
V4Vince <vince.guoo@gmail.com>
2016-08-25 20:12:18 -0400

    Refactors contract
    -Move actions and buttons from all-forms/new-contract to contract-card
    -Changes modal to fixed footer in contract-card
    -Adjust modal with and height
    
    -Removes modal-window component

--- app/components/contract-card/component.js
+++ app/components/contract-card/component.js
@@ -1,18 +1,57 @@
+//TO DO:
+// -Use a date helper so that choosing a date in the form will be in the correct format to post to backend
+
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  flashMessages: Ember.inject.service(),
+  store: Ember.inject.service(),
   //unitData <- from unit-details component
   //showContract <- from unit-details component
   //showContractForm <- from unit-details component
+  contractParams: {},
+
   actions: {
 
-    load: function(){
-      let unit = this.get('unitData');
-      console.log(unit);
-      let contract = unit.get('contract');
-      console.log(contract);
+    createContract () {
+      let contractParams = this.get('contractParams');
+      let contract = this.get('store').createRecord('contract', contractParams);
+      contract.set('unit', this.get('unitData'));
+      contract.save()
+      .then(() => {
+        this.set('showContractForm', false);
+      })
+      .then(() => {
+        this.get('flashMessages')
+        .success('Successfully added a new contract');
+      })
+      .catch((data) => {
+        console.log(data);
+        this.get('flashMessages')
+        .danger('There was a problem. Please try again.');
+      });
     },
+
+    deleteContract: function(){
+      let contract = this.get('unitData.contract');
+      console.log(contract);
+      contract.destroyRecord()
+      .then(() => {
+        this.set('showContract', false);
+      })
+      .then(() => {
+        this.get('flashMessages')
+        .success('Successfully terminated the current contract for this unit');
+      })
+      .catch((data) => {
+        console.log(data);
+        this.get('flashMessages')
+        .danger('There was a problem. Please try again.');
+      });
+    },
+
     closeModal: function(){
+      this.set('contractParams', {});
       this.set('showContract', false);
       this.set('showContractForm', false);
     },
