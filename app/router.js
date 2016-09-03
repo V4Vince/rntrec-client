907612b5e1597ef9cfadbb7bb432a3939592ccf5
V4Vince <vince.guoo@gmail.com>
2016-09-02 20:14:26 -0400

    Removes expenses route

--- app/router.js
+++ app/router.js
@@ -15,7 +15,6 @@ Router.map(function () {
   this.route('units');
   this.route('unit', { path: '/units/:unit_id' });
   this.route('new-property');
-  this.route('expenses');
   this.route('profile');
 });
 
