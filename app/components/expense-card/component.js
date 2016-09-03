66cc24175afd69a6f5aac2c6c04e23171f8656a4
V4Vince <vince.guoo@gmail.com>
2016-09-03 14:04:58 -0400

    Adds ability to post to shallow route expenses

--- app/components/expense-card/component.js
+++ app/components/expense-card/component.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   flashMessages: Ember.inject.service(),
   store: Ember.inject.service(),
+  ajax: Ember.inject.service(),
   //unitData <- from unit-details component
   //showContract <- from unit-details component
 
@@ -17,31 +18,78 @@ export default Ember.Component.extend({
     this.set('showExpenseForm', false);
   },
 
-
   actions: {
 
     createExpense () {
-      let expenseParams = this.get('expenseParams');
-      let expense = this.get('store').createRecord('expense', expenseParams);
       if (this.get('house')) {
-        expense.set('house', this.get('house'));
+        let params = this.get('expenseParams');
+        let url = `/houses/${this.get('house.id')}/expenses`;
+        console.log(params);
+        console.log(url);
+        return this.get('ajax').post(url, {
+          data: {
+            expense: {
+              expense_for: params.expense_for,
+              expense_description: params.expense_description,
+              expense_amount: params.expense_amount,
+              expense_date: params.expense_date
+            },
+          },
+        });
       } else {
-        expense.set('unit', this.get('unit'));
+        let params = this.get('expenseParams');
+        let url = `/units/${this.get('unit.id')}/expenses`;
+        console.log(params);
+        console.log(url);
+        return this.get('ajax').post(url, {
+          data: {
+            expense: {
+              expense_for: params.expense_for,
+              expense_description: params.expense_description,
+              expense_amount: params.expense_amount,
+              expense_date: params.expense_date
+            },
+          },
+        });
       }
-      return expense.save()
-      .then(() => {
-        this.resetForm();
-      })
-      .then(() => {
-        this.get('flashMessages')
-        .success('Successfully added a new expense');
-      })
-      .catch((data) => {
-        console.log(data);
-        this.get('flashMessages')
-        .danger('There was a problem. Please try again.');
-      });
+      // .then(() => {
+      //   this.resetForm();
+      // })
+      // .then(() => {
+      //   this.get('flashMessages')
+      //   .success('Successfully added a new expense');
+      // })
+      // .catch((data) => {
+      //   console.log(data);
+      //   this.get('flashMessages')
+      //   .danger('There was a problem. Please try again.');
+      // });
     },
+
+
+
+    // createExpense () {
+    //   let expenseParams = this.get('expenseParams');
+    //   let expense = this.get('store').createRecord('expense', expenseParams);
+    //   if (this.get('house')) {
+    //     expense.set('house', this.get('house'));
+    //   } else {
+    //     expense.set('unit', this.get('unit'));
+    //   }
+    //   return expense.save()
+    //   .then(() => {
+    //     this.resetForm();
+    //   })
+    //   .then(() => {
+    //     this.get('flashMessages')
+    //     .success('Successfully added a new expense');
+    //   })
+    //   .catch((data) => {
+    //     console.log(data);
+    //     this.get('flashMessages')
+    //     .danger('There was a problem. Please try again.');
+    //   });
+    // },
 
     // deleteExpense: function(){
     //   let house = this.get('house');
