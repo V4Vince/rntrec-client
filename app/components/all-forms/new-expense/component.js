d8520597f05a8a4bea680cfb774db6d2f131c4a8
V4Vince <vince.guoo@gmail.com>
2016-09-18 19:52:32 -0400

    Successfully mask currency input value
    -Installs ember text-mask add-on
    -Adds currency mask to expense-card
    
    TO DO:
    -must remove the mask before persisting the value to back-end

--- app/components/all-forms/new-expense/component.js
+++ app/components/all-forms/new-expense/component.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
+import { createNumberMask } from 'ember-text-mask-addons';
 
 export default Ember.Component.extend({
+
+//TO Do:
+//Create a function that takes the result from the masked input and mutate it to match the format Rails is expecting.
+//USER INPUT: $1,200 -> RAILS: 1200
+  createNumberMask: createNumberMask({
+    allowDecimal: true
+  })
+
 });
