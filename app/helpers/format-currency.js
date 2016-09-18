5823bd1a8144dd94cc3a4a6a0908eb026800fc55
V4Vince <vince.guoo@gmail.com>
2016-09-18 17:36:35 -0400

    Adds inserting commas to currency helper

--- app/helpers/format-currency.js
+++ app/helpers/format-currency.js
@@ -1,12 +1,24 @@
 import Ember from 'ember';
 
-export function formatCurrency([value]) {
-  let dollars = Math.floor(value / 100);
-  let cents = value % 100;
-  let sign = '$';
-
-  if (cents.toString().length === 1) { cents = '0' + cents; }
-  return `${sign}${dollars}.${cents}`;
+//Adds commas after 3 digits from the back
+function commafy( num ) {
+    let noCommaNum = num.toString();
+    if (noCommaNum.length >= 4) {
+        let newNum = noCommaNum.slice(0, -3) + "," + noCommaNum.slice(-3);
+        return newNum;
+    }
 }
 
+export function formatCurrency([value]) {
+  if (value !== undefined || !value.isNaN()) {
+
+    let dollars = Math.floor(value / 100);
+    let dollarsWithComma = commafy(dollars);
+    let cents = value % 100;
+    let sign = '$';
+
+    if (cents.toString().length === 1) { cents = '0' + cents; }
+    return `${sign}${dollarsWithComma}.${cents}`;
+    }
+  }
 export default Ember.Helper.helper(formatCurrency);
