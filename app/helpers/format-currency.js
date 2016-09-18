9560d8f362f546a1212c8f1385a91db20ab51714
V4Vince <vince.guoo@gmail.com>
2016-09-18 17:49:12 -0400

    Edge cases for no security deposit contract-card
    -If there is no security deposit, will print out 'No security deposit for this unit'
    
    -Removes if(undefined) from currency helper

--- app/helpers/format-currency.js
+++ app/helpers/format-currency.js
@@ -10,7 +10,6 @@ function commafy( num ) {
 }
 
 export function formatCurrency([value]) {
-  if (value !== undefined || !value.isNaN()) {
 
     let dollars = Math.floor(value / 100);
     let dollarsWithComma = commafy(dollars);
@@ -20,5 +19,5 @@ export function formatCurrency([value]) {
     if (cents.toString().length === 1) { cents = '0' + cents; }
     return `${sign}${dollarsWithComma}.${cents}`;
     }
-  }
+
 export default Ember.Helper.helper(formatCurrency);
