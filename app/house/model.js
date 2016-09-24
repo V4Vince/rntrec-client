82b2cb9841e207ba0141f50209c803d1b1eae124
V4Vince <vince.guoo@gmail.com>
2016-09-24 10:54:19 -0400

    Note to house model for computing unitExpenses
    -Unable to implement computed property in house model that sums all expenses from all unit model's expenses

--- app/house/model.js
+++ app/house/model.js
@@ -15,6 +15,10 @@ export default DS.Model.extend({
   expenses: DS.hasMany('expense', { async: false }),
 
   allExpenses: Ember.computed.mapBy('expenses', 'expenseAmount'),
-  totalExpenses: Ember.computed.sum('allExpenses')
+  totalExpenses: Ember.computed.sum('allExpenses'),
+
+  //this calculates the total expense of all units. ISSUE: will only calculate after the unit route is accessed. Will display $0 before unit route is accesses or if page is refreshed
+  allUnitExpenses: Ember.computed.mapBy('units', 'totalExpenses'),
+  totalUnitExpenses: Ember.computed.sum('allUnitExpenses'),
 
 });
