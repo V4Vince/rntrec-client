52afceec4ec87045c524c450570a9c861f41b914
V4Vince <vince.guoo@gmail.com>
2016-10-06 12:16:28 -0400

    Separates option-buttons component into 2
    -option-buttons into house-option-buttons and unit-option-buttons

--- tests/integration/components/property-overview-modal/unit-option-buttons/component-test.js
+++ tests/integration/components/property-overview-modal/unit-option-buttons/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('property-overview-modal/option-buttons', 'Integration | Component | property overview modal/option buttons', {
+moduleForComponent('property-overview-modal/unit-option-buttons', 'Integration | Component | property overview modal/unit option buttons', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{property-overview-modal/option-buttons}}`);
+  this.render(hbs`{{property-overview-modal/unit-option-buttons}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#property-overview-modal/option-buttons}}
+    {{#property-overview-modal/unit-option-buttons}}
       template block text
-    {{/property-overview-modal/option-buttons}}
+    {{/property-overview-modal/unit-option-buttons}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
