96c02c557716d5f2b1a84f30d8024ddecaccd6d8
V4Vince <vince.guoo@gmail.com>
2016-10-17 20:47:28 -0400

    Adds new expense-history component & migrate temp

--- app/components/unit-details/money-details/component.js
+++ app/components/unit-details/money-details/component.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  expenseHistory: false,
+  actions: {
+    showExpenseHistory(){
+      this.set('expenseHistory', true);
+      console.log(this.get('expenseHistory'));
+    },
+  }
 });
